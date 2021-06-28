be7798c80527c2e12ae6edc668227a536634ca54
mitpal <mitpal16@gmail.com>
2021-06-28 17:28:07 +0300

    first upload first exam done settings for counter (status: in progress)

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,68 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Display from "./Component/Display/Display";
+import styles from './App.module.css'
+import Settings from "./Component/Settings/Settings";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    let [counter, setCounter] = React.useState(0)
+    let [disable, setDisable] = React.useState(true)
+    let [active, setActive] = React.useState(false)
+    let [maxValue, setMaxValue] = React.useState(5)
+    let [minValue, setMinValue] = React.useState(0)
+
+    let inc = () => {
+        if (counter >= 0 && counter < 5) {
+            setCounter(++counter)
+            setDisable(false)
+        }
+        if (counter === 5) {
+            setActive(true)
+        }
+    }
+    let reset = () => {
+        setCounter(0)
+        setDisable(true)
+        setActive(false)
+    }
+
+
+    const changeMaxValue = (maxValue: number) => {
+        setMaxValue(maxValue)
+    }
+    const changeMinValue = (minValue: number) => {
+        setMinValue(minValue)
+    }
+
+    const onClickSetHandler = () => {
+        setCounter(5)
+    }
+
+    const onClickHandler = () => inc()
+    const onResetHandler = () => reset()
+
+    return (
+        <div className={styles.App}>
+            <div className={styles.element__item}>
+                <div className={styles.counter__display}>
+                    <Settings maxValue={maxValue} minValue={minValue} changeMaxValue={changeMaxValue}
+                              changeMinValue={changeMinValue}/>
+                </div>
+                <div>
+                    <button onClick={onClickSetHandler}>Set</button>
+                </div>
+            </div>
+            <div className={styles.element__item}>
+                <div className={styles.counter__display}>
+                    <Display count={counter} active={active}/>
+                </div>
+                <div>
+                    <button onClick={onClickHandler}>Increase</button>
+                    <button disabled={disable} onClick={onResetHandler}>Reset</button>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
